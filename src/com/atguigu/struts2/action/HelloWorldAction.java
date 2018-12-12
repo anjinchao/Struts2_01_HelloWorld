bbafd8d81978374699f916b8ce080d324ea43abd
anjinchao <anjinchao91@163.com>
2018-12-12 20:53:46 +0800

    555

--- src/com/atguigu/struts2/action/HelloWorldAction.java
+++ src/com/atguigu/struts2/action/HelloWorldAction.java
@@ -14,7 +14,7 @@ public class HelloWorldAction {
 	 */
 	public String hello() throws Exception{
 		
-		System.out.println("Hello,Struts2!11114444");
+		System.out.println("Hello,Struts2!11114444555");
 		
 		return "success";
 	}
