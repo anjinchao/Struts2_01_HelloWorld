a739df652079160c81f2c598f43612e913e9aa15
anjinchao <anjinchao91@163.com>
2018-12-12 17:30:40 +0800

    init

---
+++ src/com/atguigu/struts2/action/HelloWorldAction.java
@@ -0,0 +1,22 @@
+package com.atguigu.struts2.action;
+
+//代替Servlet处理客户端请求
+public class HelloWorldAction {
+	
+	/**
+	 * 必须是public
+	 * 必须是返回类型String
+	 * 必须是无参数的
+	 * 方法名称：默认是execute
+	 * 异常声明可以省略
+	 * @return
+	 * @throws Exception
+	 */
+	public String hello() throws Exception{
+		
+		System.out.println("Hello,Struts2!");
+		
+		return "success";
+	}
+	
+}
